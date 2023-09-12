d2adff2dd0d29dbab6f96ab91a3dd98bf607ddc7
Levin <levsza13@gmail.com>
2023-09-12 19:29:01 +0200

    5

--- 1ora/fifo/FixedQueue.h
+++ 1ora/fifo/FixedQueue.h
@@ -5,20 +5,20 @@
 
 class FixedQueue {
 public:
-  FixedQueue();
-  ~FixedQueue();
-  void in(int new_item);
-  int out();
-  int first() const;
-  bool isEmpty() const;
-  bool isFull() const;
-  void print() const;
+    FixedQueue();
+    ~FixedQueue();
+    void in(int new_item);
+    int out();
+    int first() const;
+    bool isEmpty() const;
+    bool isFull() const;
+    void print() const;
 
 private:
-  static const int CAPACITY = 10;
-  int array[CAPACITY];
-  int head, tail; // sor első eleme, sor első szabad helye
-  bool empty; // ha az előző kettő egyenlő akkor a sor vagy üres vagy teli
+    static const int CAPACITY = 10;
+    int array[CAPACITY];
+    int head, tail; // sor első eleme, sor első szabad helye
+    bool empty; // ha az előző kettő egyenlő akkor a sor vagy üres vagy teli
 };
 
 #endif /* FIXEDQUEUE_H_ */
