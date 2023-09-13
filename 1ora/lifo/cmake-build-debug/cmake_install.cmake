ea7157616ecb68527e035d6a3ab4c7720c1a4ae6
Levin <levsza13@gmail.com>
2023-09-13 18:50:24 +0200

    7 gyakorlas otthon sor befejezve

--- 1ora/lifo/cmake-build-debug/cmake_install.cmake
+++ 1ora/lifo/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Egyetem2/3felev/Adatszerkezetek/teszt1
+# Install script for directory: D:/Egyetem2/3felev/Adatszerkezetek/1ora/lifo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Egyetem2/3felev/Adatszerkezetek/teszt1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Egyetem2/3felev/Adatszerkezetek/1ora/lifo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
