bfcbddbc404b26b907e1ba346210d079a34c3311
Levin <levsza13@gmail.com>
2023-09-23 16:28:10 +0200

    hf2 FINALLY eslő verzio

---
+++ hf2/lengyel/PolishNotation.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Levi on 2023. 09. 23..
+//
+
+#include "PolishNotation.h"
+
+
+
+PolishNotation::PolishNotation() {
+
+
+
+}
+
+
+string PolishNotation::convert(string polishInfix) {
+
+    return "0";
+}
---
+++ hf2/lengyel/PolishNotation.h
@@ -0,0 +1,23 @@
+//
+// Created by Levi on 2023. 09. 23..
+//
+
+#ifndef LENGYEL_POLISHNOTATION_H
+#define LENGYEL_POLISHNOTATION_H
+
+#include <string>
+using namespace std;
+
+class PolishNotation {
+
+public:
+    PolishNotation();
+    //~PolishNotation();
+    string convert(string polishInfix);
+    int evaluate(string polish_input);
+private:
+    int ConvertDecimal(string number);
+
+
+};
+#endif //LENGYEL_POLISHNOTATION_H
