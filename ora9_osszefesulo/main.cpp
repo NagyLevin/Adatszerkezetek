9bb5eba168fd3ede70a5ba080fe102ee3206f10c
Levin <levsza13@gmail.com>
2023-11-14 10:10:34 +0100

    osszefesules oran befejezve valami meg nem 100/100

--- ora9_osszefesulo/main.cpp
+++ ora9_osszefesulo/main.cpp
@@ -8,21 +8,57 @@ using namespace std;
 
 /// Osszefesul ket rendezett sorozatot. //feltesszuk hogy rendezetttek
 void mergeArrays(const int vec1[], size_t n1, const int vec2[], size_t n2, int result[]) {
-	// TODO
-    (void)vec1;
-    (void)n1;
-    (void)vec2;
-    (void)n2;
-    (void)result;
+	unsigned int it1,it2,it3 = 0;
+    while (it1 < n1 &&  it2 < n2){
+        if(vec1[it1] < vec2[it2]){
+            result[it3++] = vec1[it1++]; //kiolvassuk majd növeljuk az erteket
+        }
+        else if(vec1[it1] > vec2[it2]){ //masik nagyobb
+            result[it3++] = vec2[it2++];
+        }
+        else if(vec1[it1] == vec2[it2]){   //egyenloek
+            result[it3++] = vec1[it1++];
+            result[it3++] = vec2[it2++];
+
+        }
+
+    }
+    while (it1 < n1 ){  //ha meg maradtak valamelyikben elemek
+        result[it3++] = vec1[it1++];
+
+    }
+    while (it2 < n2 ){
+        result[it3++] = vec2[it2++];
+
+    }
+
+
+
 }
 
 void mergeSort(int vec[], size_t n) {
-	// TODO
+	if(n <2){
+        return;
+    }
+    int n1 = n/2 , n2 = n-n1;
+
+    mergeSort(vec,n1);  //vektor ket reszre bontasa
+    mergeSort(vec+n1,n2);   //vektor ket reszre bontasa
+
+    int elsofel[n1] , masodikfel[n2];
+
+    for (int i = 0; i < n1; ++i) {
+        elsofel[i] = vec[i];
+
+    }
+    for (int i = 0; i < n2; ++i) {
+        masodikfel[i] = vec[n1+i];//n1 el eltolva nezzuk az ertekeket
+
+    }
+    mergeArrays(elsofel,n1,masodikfel,n2,vec);//osszefesules meghivasa
 
 
 
-    (void)vec;
-    (void)n;
 }
 
 
