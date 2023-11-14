99ed042815cee0cb20d7b5129594e210d1215b83
Levin <levsza13@gmail.com>
2023-11-14 08:47:27 +0100

    kupacrendez2 es osszefesuleses elkezdve

---
+++ ora9_kupacrendez2/heap_sort.hpp
@@ -0,0 +1,29 @@
+
+/**
+ *
+ * @file heap_sort.hpp
+ * @author tekda
+ * Heapsort
+ *
+ */
+
+#ifndef HEAP_SORT_HPP
+#define HEAP_SORT_HPP
+
+#include "heap.hpp"
+
+/**
+ *
+ * Kupaccal rendező eljárás
+ *
+ * @param v rendezendő vektor
+ *
+ */
+
+template <class T> void heapSort(std::vector<T> &v) {
+  Heap<T> heap(v);
+
+
+}
+
+#endif // HEAP_SORT_HPP
---
+++ ora9_osszefesulo/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+/// Osszefesul ket rendezett sorozatot.
+void mergeArrays(const int vec1[], size_t n1, const int vec2[], size_t n2, int result[]) {
+	// TODO
+    (void)vec1;
+    (void)n1;
+    (void)vec2;
+    (void)n2;
+    (void)result;
+}
+
+void mergeSort(int vec[], size_t n) {
+	// TODO
+    (void)vec;
+    (void)n;
+}
+
+
+int main() {
+	srand(time(NULL));
+	const std::size_t LARGE_TEST_SIZE = 100000;
+	/// MergeSort
+	cout << "mergeSort tesztelese:" << endl;
+	const int n = 11;
+	int t[n] = { 123, 2, -3, 9998, 10, 15, -12, -120, 112, 998, 0 };
+	mergeSort(t, n);
+	for (int i = 0; i < n; i++)
+		cout << t[i] << ' ';
+	std::cout << std::endl;
+	std::cout << "mergeSort : " << (isSorted(t, t + n) ? "OK" : "NOK")
+			<< std::endl;
+	std::cout << "Nagyobb teszt : " << std::flush;
+	int * tlarge = new int[LARGE_TEST_SIZE];
+	for (std::size_t i = 0; i < LARGE_TEST_SIZE; ++i) {
+		tlarge[i] = rand();
+	}
+	mergeSort(tlarge, LARGE_TEST_SIZE);
+
+	std::cout << (isSorted(tlarge, tlarge + LARGE_TEST_SIZE) ? "OK" : "NOK")
+			<< std::endl;
+	delete[] tlarge;
+
+}
