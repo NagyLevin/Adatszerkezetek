b2065247934f4517bb55cf3a7bad84ef5cc0c9fb
Levin <levsza13@gmail.com>
2023-12-11 16:27:18 +0100

    Potzhra gyakorlas2

--- potzh/main.cpp
+++ potzh/main.cpp
@@ -2,7 +2,11 @@
 #include "vector"
 #include "FixedSor.h"
 #include "FixedVerem.h"
-
+#include "DinamicVerem.h"
+#include "DinamicSor.h"
+#include "2xList.h"
+#include "binfa.h"
+#include "awltree.h"
 
 using namespace std;
 
@@ -20,15 +24,34 @@ int main() {
     verem.push(1);
 
     //Dinamic Stack
+    DynamicIntStack DinamicVerem;
+    DinamicVerem.push(10);
+    DinamicVerem.print();
+
     //Dinamic Queue
+    DynamicIntQueue DinamicSor;
+    DinamicSor.in(11);
+    DinamicSor.print();
+
 
     //zhpelda
 
-    //Lancoltlista
+    //2xLancoltlista
+    List<int> lista;
+
+    lista.insertFirst(10);
+    lista.print();
 
     //Binarfa
+    Bs_tree<int> binarfa;
+    //binarfa.insert(1);
+    //binarfa.find(1);
 
     //avlfa
+    AvlTree<int> awl;
+    awl.insert(13);
+    awl.getRoot();
+
 
     //piros fekete?
 
